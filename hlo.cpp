29ed3953beeac3b73e3090bff87afb02394abbb4
Madhavan Dixit <madhavanep1cc@gmail.com>
2025-04-10 22:20:53 +0530

    Add files via upload

---
+++ hlo.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int rollNumber;
+    float cgpa;
+    vector<string> courses;
+
+public:
+    Student(string n, int r, float c) {
+        name = n;
+        rollNumber = r;
+        cgpa = (c >= 0.0 && c <= 4.0) ? c : 0.0;
+    }
+
+    void addCourse(string course) {
+        courses.push_back(course);
+    }
+
+    void updateCGPA(float newCgpa) {
+        if (newCgpa >= 0.0 && newCgpa <= 4.0)
+            cgpa = newCgpa;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Roll Number: " << rollNumber << endl;
+        cout << "CGPA: " << cgpa << endl;
+        cout << "Courses: ";
+        for (string course : courses) {
+            cout << course << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Student s("Riya", 1001, 3.4);
+    s.addCourse("Math");
+    s.addCourse("Physics");
+    s.updateCGPA(3.6);
+    s.display();
+
+    return 0;
+}
